fcacdc636b7e52cf51989fefcc06a7d8eed42f1f
tm <984140149@qq.com>
2017-02-07 10:35:02 +0800

    创建数据库

---
+++ app/src/main/java/cn/miaolovezhen/coolweather/db/Country.java
@@ -0,0 +1,46 @@
+package cn.miaolovezhen.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by tmmac on 17/2/7.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryId;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
